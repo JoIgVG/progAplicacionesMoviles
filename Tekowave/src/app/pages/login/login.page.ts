09bf68f599016cce6e8b129d29b365ad28db1ed2
JoIgVG <163932787+JoIgVG@users.noreply.github.com>
2024-08-31 19:25:12 -0400

    Add files via upload

---
+++ Tekowave/src/app/pages/login/login.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  public titulo:String="Log-In";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
